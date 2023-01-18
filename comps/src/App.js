309fef77968903df5c0bbad5f8a078ae34b106c4
Diana Palafox <dianapalafox95@gmail.com>
2023-01-18 14:49:03 -0600

    Add Button component and validate props with PropType

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,7 +1,24 @@
+import Button from "./Button";
 
 function App() {
     return(
-        <div>App</div>
+        <div>
+            <div>
+                <Button success rounded outline>Click me!</Button>
+            </div>
+            <div>
+                <Button danger outline>Buy now!</Button>
+            </div>
+            <div>
+                <Button warning>See deal!</Button>
+            </div>
+            <div>
+                <Button secondary>Hide adds!</Button>
+            </div>
+            <div>
+                <Button success>Add to cart!</Button>
+            </div>
+        </div>
     )
 }
 
